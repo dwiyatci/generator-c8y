e05218c43ef2c05397c4e461de55bd2e2437cff3
Glenn Dwiyatcita <dwiyatci@mail.com>
2018-09-01 10:24:49 +0200

    ✨ feat(testing): fancy school of testing that never calls beforeEach()

--- generators/plugin/templates/hello/hello.component.spec.js
+++ generators/plugin/templates/hello/hello.component.spec.js
@@ -1,33 +1,14 @@
 describe('c8y.pocs.hello: c8yHello component', () => {
   'use strict';
 
-  let $injector;
-  let $rootScope;
-  let $compile;
-  let $componentController;
-
-  beforeEach(() => {
-    common.globalBeforeWithUI();
-    module('c8y.pocs.hello');
-
-    inject((_$injector_) => {
-      $injector = _$injector_;
-      $rootScope = $injector.get('$rootScope');
-      $compile = $injector.get('$compile');
-      $componentController = $injector.get('$componentController');
-    });
-  });
-
   it('component should exist', () => {
+    const { $injector } = setup();
+
     expect($injector.has('c8yHelloDirective'))
       .toEqual(true);
   });
 
   describe('displaying text', () => {
-    beforeEach(() => {
-      // dependencies to be stubbed
-    });
-
     it('should display "hello, world" by default', () => {
       // given
       const textBinding = undefined;
@@ -43,6 +24,8 @@ describe('c8y.pocs.hello: c8yHello component', () => {
     });
 
     function testDisplayingText(textBinding, expectedText) {
+      const { $componentController } = setup();
+
       // when
       const controller = $componentController('c8yHello', undefined, { text: textBinding });
       controller.$onInit();
@@ -53,12 +36,14 @@ describe('c8y.pocs.hello: c8yHello component', () => {
     }
 
     it('you should compile the component instead if use case getting too complex', () => {
+      const { $rootScope, $compile } = setup();
+
       // given
       const bindings = { world: 'world' };
       const template = '<c8y-hello world="world"></c8y-hello>';
 
       // when
-      const element = createComponent(template, bindings);
+      const element = createComponent({ $rootScope, $compile, template, bindings });
 
       // then
       expect(element.html())
@@ -66,14 +51,31 @@ describe('c8y.pocs.hello: c8yHello component', () => {
       expect(element.controller('c8yHello').text)
         .toEqual('hello, world');
     });
-
-    function createComponent(template, bindings) {
-      const $scope = _.assign($rootScope.$new(), bindings);
-
-      const element = $compile(template)($scope);
-      $scope.$apply();
-
-      return element;
-    }
   });
+
+  function setup() {
+    const setupVariables = {};
+
+    common.globalBeforeWithUI();
+    angular.mock.module('c8y.pocs.hello');
+
+    inject(($injector) => _.assign(setupVariables, {
+      $injector,
+      $rootScope: $injector.get('$rootScope'),
+      $q: $injector.get('$q'),
+      $compile: $injector.get('$compile'),
+      $componentController: $injector.get('$componentController')
+    }));
+
+    return setupVariables;
+  }
+
+  function createComponent({ $rootScope, $compile, template, bindings }) {
+    const $scope = _.assign($rootScope.$new(), bindings);
+
+    const element = $compile(template)($scope);
+    $scope.$apply();
+
+    return element;
+  }
 });
